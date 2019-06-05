cd49400c80b83638e98090a740381f5bd8b9313d
gladydennyson <gladydennyson184@gmail.com>
2019-06-05 23:07:03 +0530

    Printing and storing csv data entries as arrays

---
+++ arrayformat.py
@@ -0,0 +1,36 @@
+from array import *
+import csv
+Topics = []
+sublist = []
+#slistcount = 0
+numelements = 0
+count = 0 
+with open('tweakspeaking.csv') as csvfile:
+	readCSV = csv.reader(csvfile, delimiter=',')
+
+	for row in readCSV:
+		if(row[0] != ''):
+			#print (row[0])
+			Topics.append(row[0])
+			
+
+		
+		if count != 15 :
+			
+			sublist.append(row[1])
+			
+			count = count+1
+			numelements = len(sublist)
+			if numelements == 15:
+				print(sublist)
+				print("______________________________________________________________")
+				#file1 = open("arraywrite.txt","a") 
+				#file1.write(sublist)
+				#file1.close()
+			
+		else:
+			sublist = []
+			sublist.append(row[1])
+			count = 0
+			count = count+1
+	print(Topics)
